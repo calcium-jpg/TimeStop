1d34c831e7ec4d5bbb6dd61827f0ec2145dc6865
calcium-jpg <calvinthompson07@protonmail.com>
2024-06-29 15:38:35 -0500

    Version 1.0

--- src/main/java/place/calcium/timestop/PlayerListener.java
+++ src/main/java/place/calcium/timestop/PlayerListener.java
@@ -1,4 +1,39 @@
 package place.calcium.timestop;
 
-public class PlayerListener {
+import org.bukkit.GameRule;
+import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerListener implements Listener {
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        Server server = player.getServer();
+
+        if (server.getOnlinePlayers().size() == 1) {
+            server.getLogger().info("Last player left, daylight cycle set to false!");
+
+            for (World world : server.getWorlds()) {
+                world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Server server = player.getServer();
+
+        server.getLogger().info("Player joined, daylight cycle set to true!");
+
+        for (World world : server.getWorlds()) {
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
+        }
+
+    }
 }
--- src/main/java/place/calcium/timestop/TimeStop.java
+++ src/main/java/place/calcium/timestop/TimeStop.java
@@ -1,17 +1,18 @@
 package place.calcium.timestop;
 
+import org.bukkit.GameRule;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TimeStop extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getLogger().info("Daylight cycle set to false initially!");
 
-    }
+        for (World world : getServer().getWorlds()) {
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+        }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
     }
 }
